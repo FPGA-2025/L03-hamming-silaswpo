6a21767f41a332c76bea9e50a053465277e4d0df
silaswpo <silaswesleypereiradeoliveira@gmail.com>
2025-04-01 22:36:19 -0300

    Adicionando módulos atualizados do código de Hamming e injeção de erro tarefa 6_corrigindo

--- corrige_hamming.v
+++ corrige_hamming.v
@@ -1,6 +1,6 @@
 module corrige_hamming (
   input [14:0] entrada, // a paridade é o bit mais significativo (dado[8])
-  output reg [10:0] saida
+  output  [10:0] saida
 );
 wire [3:0] erro;
 
@@ -11,7 +11,7 @@ assign erro[2] = entrada[3] ^ entrada[4] ^ entrada[5] ^ entrada[6] ^ entrada[11]
 assign erro[3] = entrada[7] ^ entrada[8] ^ entrada[9] ^ entrada[10] ^ entrada[11] ^ entrada[12] ^ entrada[13] ^ entrada[14];
 
 // Corrige o erro invertendo o bit errado (se houver erro)
-wire [14:0] recebido_corrigido = (erro != 0) ? entrada ^ (15'b000000000000001 << (erro - 1)) : receentradabido;
+wire [14:0] recebido_corrigido = (erro != 0) ? recebido ^ (15'b000000000000001 << (erro - 1)) : recebido;
 
 // extrai os dados corrigidos
 assign saida = {recebido_corrigido[14:12], recebido_corrigido[11:8], recebido_corrigido[6:4], recebido_corrigido[2]};
--- injetor.v
+++ injetor.v
@@ -2,7 +2,7 @@ module injetor(
   input [14:0] entrada,
   input [3:0] n,
   input erro,
-  output reg [14:0] saida
+  output [14:0] saida
 );
 
 assign saida = erro ? entrada ^ (15'b000000000000001 << n) : entrada;
